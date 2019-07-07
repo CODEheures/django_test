fb81496fd756e4538802097441bd41f7bbea080e
CODEheures <webmaster@codeheures.fr>
2019-07-07 13:26:57 +0200

    [UPDATE] models with ORM

--- disquaire_project/store/views.py
+++ disquaire_project/store/views.py
@@ -1,39 +1,47 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-from .models import ALBUMS
+from .models import Album, Artist, Contact, Booking
 # Create your views here.
 
 
 def index(request):
-    message = "Salut tout le monde!"
+    albums = Album.objects.filter(available=True).order_by('-created_at')[:4]
+    formated_albums = ["<li><a href='/store/album/{}'>{}</a></li>"
+                           .format(album.id, album.title) for album in albums]
+    message = "<ul>{}</ul>".format("\n".join(formated_albums))
+
     return HttpResponse(message)
 
 
 def listing(request):
-    albums = ["<li><a href='/store/album/{}'>{}</a></li>"
-              .format(ind, album['name']) for ind, album in enumerate(ALBUMS)]
-    message = "<ul>{}</ul>".format("\n".join(albums))
+    albums = Album.objects.filter(available=True)
+    formated_albums = ["<li><a href='/store/album/{}'>{}</a></li>"
+              .format(album.id, album.title) for album in albums]
+    message = "<ul>{}</ul>".format("\n".join(formated_albums))
 
     return HttpResponse(message)
 
 
 def detail(request, album_id):
-    album = ALBUMS[int(album_id)]
-    artists = " ".join(artist['name'] for artist in album['artists'])
-    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album['name'], artists)
+    album = Album.objects.get(pk=int(album_id))
+    artists = " ".join(artist.name for artist in album.artists.all())
+    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album.title, artists)
     return HttpResponse(message)
 
 def search(request):
     query = request.GET['query']
     if not query:
-        message = "Faites au moins l'effort de taper une lettre pour la recherche!"
+        albums = Album.objects.all()
     else:
-        albums = [album for album in ALBUMS if query in " ".join(artist['name'] for artist in album['artists'])]
+        albums = Album.objects.filter(title__icontains=query)
 
-        if len(albums) == 0:
-            message = "Aucun album ne correspond à votre recherche"
-        else:
-            liste_html = ["<li>{}</li>".format(album['name']) for album in albums]
-            message = "Votre recherche: <ul>{}</ul>".format("\n".join(liste_html))
+    if not albums.exists():
+        albums = Album.objects.filter(artists__name__icontains=query)
+
+    if not albums.exists():
+        message = "Aucun album ne correspond à votre recherche"
+    else:
+        liste_html = ["<li>{}</li>".format(album.title) for album in albums]
+        message = "Votre recherche: <ul>{}</ul>".format("\n".join(liste_html))
 
     return HttpResponse(message)
